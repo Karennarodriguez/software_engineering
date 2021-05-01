de22585cd3db531bc6d6a5bc33ade9a6bd522123
Mousta Taenchiki <45579563+Moustadirane@users.noreply.github.com>
2021-05-01 13:53:02 -0400

    Update settings.py

--- Backyard/Backyard/settings.py
+++ Backyard/Backyard/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config # for secret key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -115,3 +116,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SECRET_KEY = config('SECRET_KEY')

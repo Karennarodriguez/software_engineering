b8501a09828a63403ba0799ea2bd8c21d7e5ca04
Mousta Taenchiki <45579563+Moustadirane@users.noreply.github.com>
2021-05-01 13:47:13 -0400

    Update settings.py

--- Backyard/Backyard/settings.py
+++ Backyard/Backyard/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'uitjl(-qs710j++b0u06$*@9n@54sov-w$2s93*k9yw9sc42(s'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 

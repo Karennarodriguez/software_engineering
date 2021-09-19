09d4648158eec9220ad9ab07e9d9ff179617f894
alecasas <ac7262@nyu.edu>
2021-09-19 15:34:21 -0400

    basic comment function

--- Backyard/blog/models.py
+++ Backyard/blog/models.py
@@ -1,18 +1,20 @@
 # Create your models here.
 from django.db import models
+from django.db.models import TextField
+from django.utils import timezone
 from django.contrib.auth.models import User
 
-
 STATUS = (
-    (0,"Draft"),
-    (1,"Publish")
+    (0, "Draft"),
+    (1, "Publish")
 )
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
-    updated_on = models.DateTimeField(auto_now= True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
@@ -22,3 +24,18 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    @property
+    def number_of_comments(self):
+        return BlogComment.objects.filter(blogpost_connected=self).count()
+
+
+class BlogComment(models.Model):
+    blogpost_connected = models.ForeignKey(
+        Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return str(self.author) + ', ' + self.blogpost_connected.title[:40]
--- Backyard/blog/views.py
+++ Backyard/blog/views.py
@@ -1,12 +1,34 @@
-
 # Create your views here.
 from django.views import generic
-from .models import Post
+from .models import Post, BlogComment
+from .forms import NewCommentForm
 from django.shortcuts import render
+
+
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'home.html'
 
+
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
+    context_object_name = 'object'
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+
+        comments_connected = BlogComment.objects.filter(
+            blogpost_connected=self.get_object()).order_by('-date_posted')
+        data['comments'] = comments_connected
+        if self.request.user.is_authenticated:
+            data['comment_form'] = NewCommentForm(instance=self.request.user)
+
+        return data
+
+    def post(self, request, *args, **kwargs):
+        new_comment = BlogComment(content=request.POST.get('content'),
+                                  author=self.request.user,
+                                  blogpost_connected=self.get_object())
+        new_comment.save()
+        return self.get(self, request, *args, **kwargs)
